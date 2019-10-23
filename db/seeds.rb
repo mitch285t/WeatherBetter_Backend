d013c1d432ff934ea30c1e044e9cdfca893c0196
Anais Ahmed <anaismahmed@uchicago.edu>
2019-10-23 15:17:27 -0500

    changed weather api and added more functionality for weather info

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-austin = Location.create(name: "Austin", latitude: 30.2711, longitude: -97.7403, current_weather_url: "https://api.weather.gov/stations/katt/observations/latest", forecast_url: "https://api.weather.gov/gridpoints/EWX/155,90/forecast")
-shreveport = Location.create(name: "Shreveport", latitude: 32.3519, longitude: -93.6929, current_weather_url: "https://api.weather.gov/stations/kshv/observations/latest", forecast_url: "https://api.weather.gov/gridpoints/SHV/78,61/forecast")
-chicago = Location.create(name: "Chicago", latitude: 41.9571, longitude: -87.6551, current_weather_url: "https://api.weather.gov/stations/kord/observations/latest", forecast_url: "https://api.weather.gov/gridpoints/LOT/73,76/forecast")
+austin = Location.create(name: "Austin", latitude: 30.2711, longitude: -97.7403, forecast_url: "https://api.darksky.net/forecast/#{ENV["API_KEY"]}/30.2672,-97.7431")
+shreveport = Location.create(name: "Shreveport", latitude: 32.3519, longitude: -93.6929, forecast_url: "https://api.darksky.net/forecast/#{ENV["API_KEY"]}/32.3519,-93.6929")
+chicago = Location.create(name: "Chicago", latitude: 41.9571, longitude: -87.6551, forecast_url: "https://api.darksky.net/forecast/#{ENV["API_KEY"]}/41.9571,-87.6551")
