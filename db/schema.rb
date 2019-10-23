0bd222befb4be161d161a78b481d54519cffd83e
Mitchell Williams <mscottwilliams1994@gmail.com>
2019-10-23 15:22:15 -0500

    added migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_185817) do
+ActiveRecord::Schema.define(version: 2019_10_23_160512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_10_21_185817) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "wallet"
   end
 
 end
